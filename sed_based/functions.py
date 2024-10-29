1a1861b34dc85a8a078b7ac7afa37fa91bf9329a
Marek <mareksz192@gmail.com>
2024-10-29 15:10:21 +0100

    update
    5 class 2 timepoints classification using SMOTE , Crossvalidation added to sediment 1, update of crossvalidation funciton to functions.py

--- sed_based/functions.py
+++ sed_based/functions.py
@@ -1,14 +1,222 @@
 import pandas as pd
-
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.metrics import ConfusionMatrixDisplay
-
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tensorflow as tf
+from tensorflow import keras
 import numpy as np
+
+
+
+
+def reset_weights(model):
+    for layer in model.layers:
+        if hasattr(layer, 'kernel_initializer') and layer.kernel is not None:
+            layer.kernel.assign(layer.kernel_initializer(tf.shape(layer.kernel)))
+        if hasattr(layer, 'bias_initializer') and layer.bias is not None:
+            layer.bias.assign(layer.bias_initializer(tf.shape(layer.bias)))
+
+
+
+
+
+# crossvalidation function
+
+def crossvalidate(train_val_df, model, predictors, target, kf):
+    '''function for crossvalidation of the model
+    train_val_df - dataframe with train and validate data
+    model - model to be trained
+    predictors - list of predictors
+    target - target variable
+    kf - KFold object
+    es_callback - EarlyStopping callback
+    '''
+
+   
+    
+
+
+    i=1
+
+    acc_scores = []
+    loss_scores = []
+
+    acc_histories = []
+    loss_histories = []
+    val_acc_histories = []
+    val_loss_histories = []
+
+    global_confidence_scores = []
+
+
+    for train_index, validate_index in kf.split(train_val_df):
+        # reset weights
+        reset_weights(model)
+        
+        train_df = train_val_df.iloc[train_index]
+        validate_df = train_val_df.iloc[validate_index]
+
+        # create train and validate datasets 
+        X_train = train_df[predictors].values
+        y_train = train_df[target].values
+        y_train = keras.utils.to_categorical(y_train)
+
+        X_validate = validate_df[predictors].values
+        y_validate = validate_df[target].values
+        y_validate = keras.utils.to_categorical(y_validate)
+
+        # fit the model
+        print(f"training for {i} subset")
+        history = model.fit(X_train, y_train, epochs=300, batch_size=5, validation_data=(X_validate, y_validate), verbose=0)
+        # evaluate the model
+
+        ev_results = model.evaluate(X_validate, y_validate)
+
+
+
+        #save evaluation results 
+        acc_scores.append(ev_results[1])
+        loss_scores.append(ev_results[0])
+
+        acc_histories.append(history.history['accuracy'])
+        loss_histories.append(history.history['loss'])
+        val_acc_histories.append(history.history['val_accuracy'])
+        val_loss_histories.append(history.history['val_loss'])
+
+        # prediction of the results 
+        y_pred = model.predict(X_validate)
+        y_pred_class = np.argmax(y_pred, axis=1)
+        y_true = np.argmax(y_validate, axis=1)
+        print(f"predicted classes:       {y_pred_class}")
+        print(f"true validation classes: {y_true}")
+
+        # confidence scores 
+
+        confidence_scores = []
+        for row in y_pred:
+            highest = np.max(row)
+            mean = np.mean(np.delete(row, np.argmax(row)))
+            confidence_score = round(((highest / mean)-1),4)
+            confidence_scores.append(confidence_score)
+            global_confidence_scores.append(confidence_score)
+        
+        mean_confidence = np.mean(confidence_scores)
+        sd_confidence = np.std(confidence_scores)
+
+        print(f"mean confidence score: {round(mean_confidence,4)}, sd confidence score: {round(sd_confidence,4)}")
+        print("=====================================================")
+
+        
+
+
+
+        i+=1
+
+    # calculate mean scores
+
+    mean_acc_score = np.mean(acc_scores)
+    sd_acc_score = np.std(acc_scores)
+    mean_loss_score = np.mean(loss_scores)
+    sd_loss_score = np.std(loss_scores)
+
+
+    # part to plot mean values from history
+    
+    # mean and sd values for learning curves 
+    mean_acc_histories = np.nanmean(acc_histories, axis=0)
+    sd_acc_histories = np.nanstd(acc_histories, axis=0)
+    mean_val_acc_histories = np.nanmean(val_acc_histories, axis=0)
+    sd_val_acc_histories = np.nanstd(val_acc_histories, axis=0)
+    mean_loss_histories = np.nanmean(loss_histories, axis=0)
+    sd_loss_histories = np.nanstd(loss_histories, axis=0)
+    mean_val_loss_histories = np.nanmean(val_loss_histories, axis=0)
+    sd_val_loss_histories = np.nanstd(val_loss_histories, axis=0)
+    
+
+    print(f"validation set mean accuracy: {round(mean_acc_score,4)}, sd{round(sd_acc_score,4)}, mean loss: {round(mean_loss_score,4)}, sd: {round(sd_loss_score,4)}")
+    print(f"global mean confidence score: {round(np.mean(global_confidence_scores),4)}, sd confidence score: {round(np.std(global_confidence_scores),4)}")
+    # plotting mean curves 
+
+    plt.figure(figsize=(15, 5))
+
+    # Accuracy plot
+    plt.subplot(1, 2, 1)
+    plt.plot(mean_acc_histories, label='train', color='blue')
+    plt.fill_between(range(len(mean_acc_histories)), mean_acc_histories - sd_acc_histories, mean_acc_histories + sd_acc_histories, color='blue', alpha=0.2)
+    plt.plot(mean_val_acc_histories, label='validate', color='orange')
+    plt.fill_between(range(len(mean_val_acc_histories)), mean_val_acc_histories - sd_val_acc_histories, mean_val_acc_histories + sd_val_acc_histories, color='orange', alpha=0.2)
+    plt.legend()
+    plt.title("Mean Accuracy")
+
+    # Loss plot
+    plt.subplot(1, 2, 2)
+    plt.plot(mean_loss_histories, label='train', color='blue')
+    plt.fill_between(range(len(mean_loss_histories)), mean_loss_histories - sd_loss_histories, mean_loss_histories + sd_loss_histories, color='blue', alpha=0.2)
+    plt.plot(mean_val_loss_histories, label='validate', color='orange')
+    plt.fill_between(range(len(mean_val_loss_histories)), mean_val_loss_histories - sd_val_loss_histories, mean_val_loss_histories + sd_val_loss_histories, color='orange', alpha=0.2)
+    plt.legend()
+    plt.title("Mean Loss")
+
+    plt.show()
+
+
+    # calculating min and max values for loss plots range 
+    flat_loss_histories = np.concatenate(loss_histories)
+    flat_val_loss_histories = np.concatenate(val_loss_histories)
+    min_loss = min((min(flat_loss_histories), min(flat_val_loss_histories)))
+    max_loss = max((max(flat_loss_histories), max(flat_val_loss_histories)))
+
+
+
+    # Plotting results for each fold 
+    fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+
+
+    # Accuracy
+    for i, acc in enumerate(acc_histories):
+        axs[0, 0].plot(acc, label=f'Fold {i+1}')
+    axs[0, 0].set_title('Training Accuracy')
+    axs[0, 0].set_xlabel('Epochs')
+    axs[0, 0].set_ylabel('Accuracy')
+    axs[0, 0].set_ylim(0, 1)  # Set y-axis scale from 0 to 1
+    axs[0, 0].legend()
+
+    # Validation Accuracy
+    for i, val_acc in enumerate(val_acc_histories):
+        axs[0, 1].plot(val_acc, label=f'Fold {i+1}')
+    axs[0, 1].set_title('Validation Accuracy')
+    axs[0, 1].set_xlabel('Epochs')
+    axs[0, 1].set_ylabel('Accuracy')
+    axs[0, 1].set_ylim(0, 1)  # Set y-axis scale from 0 to 1
+    axs[0, 1].legend()
+
+    # Loss
+    for i, loss in enumerate(loss_histories):
+        axs[1, 0].plot(loss, label=f'Fold {i+1}')
+    axs[1, 0].set_title('Training Loss')
+    axs[1, 0].set_xlabel('Epochs')
+    axs[1, 0].set_ylabel('Loss')
+    axs[1, 0].set_ylim(min_loss,max_loss)  # Set y-axis scale based on the maximum loss value
+    axs[1, 0].legend()
+
+    # Validation Loss
+    for i, val_loss in enumerate(val_loss_histories):
+        axs[1, 1].plot(val_loss, label=f'Fold {i+1}')
+    axs[1, 1].set_title('Validation Loss')
+    axs[1, 1].set_xlabel('Epochs')
+    axs[1, 1].set_ylabel('Loss')
+    axs[1, 1].set_ylim(min_loss,max_loss)  # Set y-axis scale based on the maximum validation loss value
+    axs[1, 1].legend()
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+
 # Function for plotting probability for each class
 def plot_prob(y_pred):
     """Plots the highest probability out of the output vector of a model for each class."""
@@ -103,6 +311,5 @@ def pond_decoder(x,codes):
             return key
     
     return None   
-#pond_decoder("gut_14")
 
 
